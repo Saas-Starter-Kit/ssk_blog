41cf0a898bc92b92afe48503835b9f6bdd037b8c
iqbal125 <iqbal125@yahoo.com>
2021-10-06 15:02:16 -0700

    update config

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -4,9 +4,9 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula")
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
-  url: "https://your-docusaurus-test-site.com",
+  title: "React and Nodejs Boilerplate",
+  tagline: "React and Nodejs Boilerplate",
+  url: "https://saasstarterkit.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
